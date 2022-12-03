301b82526b22cf6b17f311ed88713a3644f86f33
MuadDAtr <int.stron@protonmail.com>
2022-12-03 23:59:49 +0100

    rename

--- os_test.py
+++ os_test.py
@@ -1,5 +1,10 @@
 import os
 
 print(os.getcwd())
+os.removedirs("New_directory")
+print(os.listdir())
 os.mkdir('New_directory')
+print(os.listdir())
+
+os.rename('New_directory', 'Renamed_directory')
 print(os.listdir())
