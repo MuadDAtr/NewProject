087aa8f7ec74d54f2bb7711e34c30f4d4abd447d
MuadDAtr <int.stron@protonmail.com>
2022-12-05 23:24:41 +0100

    fixin' problems

--- sys_test.py
+++ sys_test.py
@@ -1,7 +1,8 @@
 import sys
 
 print("THe scrpit you're runnin' is '%s'" %str(sys.argv[0]))
-print("There are '%s' arguments in the list  "%str(len(sys.argv)))
-
+print("There are %s arguments in the list  "%str(len(sys.argv)))
+print(" ")
 print("List of arguments: ", str(sys.argv))
+print("Third argument is:", str(sys.argv[3]))
 print(sys.argv)
