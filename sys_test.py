0a87cf792a216b9d9bb3f1d3b9fa1ad3e7569e95
MuadDAtr <int.stron@protonmail.com>
2022-12-05 23:19:27 +0100

    continuing

--- sys_test.py
+++ sys_test.py
@@ -1,1 +1,7 @@
 import sys
+
+print("THe scrpit you're runnin' is '%s'" %str(sys.argv[0]))
+print("There are '%s' arguments in the list  "%str(len(sys.argv)))
+
+print("List of arguments: ", str(sys.argv))
+print(sys.argv)
